516ac90187ebe0fdf38199ba6a3ba1013d639d74
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2023-01-05 15:19:23 +0100

    working with restapi

--- Infrastructure/SpaceStack.ts
+++ Infrastructure/SpaceStack.ts
@@ -6,14 +6,26 @@ import {
   Function as LambdaFunction,
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { RestApi } from "aws-cdk-lib/aws-apigateway";
+
+
 export class SpaceStack extends Stack {
+
+  private api =new RestApi(this, "helloApi")
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
+   new LambdaFunction(this, "helloLambda", {
       runtime: Runtime.NODEJS_18_X,
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
+    new Bucket(this, "upload-img", {
+      publicReadAccess: true,
+      bucketName: "upload-new-image",
+    });
   }
 }
+
+
