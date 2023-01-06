0f9e945bcac383aeadef27f806e22a0e514a2b84
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2023-01-06 13:42:22 +0100

    testing and debugging

--- Infrastructure/SpaceStack.ts
+++ Infrastructure/SpaceStack.ts
@@ -1,31 +1,27 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { join } from "path";
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
 import { Bucket } from "aws-cdk-lib/aws-s3";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "helloApi");
   private helloTable = new GenericTable("helloTable", "helloId", this);
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
-    // const helloLambda = new LambdaFunction(this, "helloLambda", {
-    //   runtime: Runtime.NODEJS_18_X,
-    //   code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-    //   handler: "hello.main",
-    // });
 
     const helloNodejs = new NodejsFunction(this, "helloLambda", {
       entry: join(__dirname, "..", "services", "hello", "hello.ts"),
       handler: "handler",
     });
+
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
+    helloNodejs.addToRolePolicy(s3ListPolicy);
 
     new Bucket(this, "upload-img", {
       publicReadAccess: true,
--- services/hello/hello.ts
+++ services/hello/hello.ts
@@ -1,32 +1,18 @@
 // "use strict";
+import { S3, config } from "aws-sdk";
 
-const AWS = require("aws-sdk");
-AWS.config.update({ region: "us-east-1" });
-const s3 = new AWS.S3();
 
-// Change this value to adjust the signed URL's expiration
-const URL_EXPIRATION_SECONDS = 300;
+const s3Client = new S3();
 
-const getUploadURL = async function (event: any) {
-  const randomID: string | number = Math.random() * 10000000;
-  const Key = `${randomID}.jpg`;
-  // Get signed URL from S3
-  const s3Params = {
-    Bucket: "upload-new-image",
-    Key,
-    Expires: URL_EXPIRATION_SECONDS,
-    ContentType: "image/jpeg",
-    ACL: "public-read",
-  };
-  console.log("Params: ", s3Params);
-  const uploadURL = await s3.getSignedUrlPromise("putObject", s3Params);
 
-  return JSON.stringify({
-    uploadURL: uploadURL,
-    Key,
-  });
-};
+
 async function handler(event: any) {
-  return await getUploadURL(event);
+  const { Buckets } = await s3Client.listBuckets().promise();
+  console.log("got  something out")
+  console.log(Buckets);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Buckets),
+  };
 }
 export { handler };
