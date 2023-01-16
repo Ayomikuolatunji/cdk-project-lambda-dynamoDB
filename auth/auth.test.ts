1b1ccef2fcb0808d304f580158629150300a3fcb
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2023-01-16 18:49:58 +0100

    cognito

---
+++ auth/auth.test.ts
@@ -0,0 +1,12 @@
+import { AuthService } from "./AuthService";
+import { config } from "./config";
+
+const authService = new AuthService();
+
+const user = authService.login(
+  config.TEST_USER_NAME,
+  config.TEST_USER_password
+);
+
+
+console.log(user);
---
+++ auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: "us-east-1",
+  USER_POOL_ID: "us-east-1_sFHMvFSpU",
+  APP_CLIENT_ID: "oefdobepu891pceeadhujt3mm",
+  TEST_USER_NAME: "testUser",
+  TEST_USER_password: "Password123@",
+};
